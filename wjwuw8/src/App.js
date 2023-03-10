37e1a85afa0c67418bbd715acc55ea1b533c196b
Blasterphantom <blasterneko@gmail.com>
2023-03-10 11:39:13 -0800

    git init

---
+++ wjwuw8/src/App.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import React from 'react';
+import Board from './components/boardcomponent';
+import Square from './components/squarecomponent';
+import Game from './components/gamecomponent';
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Game />
+    </div>
+  );
+}
+
+export default App;
